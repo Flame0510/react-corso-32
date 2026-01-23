93e2185da24e464c3fa817ae2cb3e472b5abdad1
Flame0510 <micheletornello5@gmail.com>
2026-01-23 17:20:49 +0100

    feat: implement theme context and provider; add theme toggling functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,49 @@ import "./App.scss";
 import Counter from "./pages/Counter/Counter";
 import UsersList from "./pages/UsersList/UsersList";
 import Home from "./pages/Home";
-import UserPage from "./pages/UserPage/UserPage";
+
 import RefPage from "./pages/RefPage";
+import UserPage from "./pages/UserPage/UserPage";
+import { useTheme } from "./hooks/useTheme";
 
 /**
  * Componente principale dell'applicazione
  * Renderizza la lista utenti e il contatore
  */
 function App() {
+    // Recupera il tema corrente e la funzione per cambiare tema dal contesto
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <BrowserRouter>
-            <nav className="flex gap-md bg-white p-sm rounded-md justify-center">
-                <Link to="/">HOME</Link>
-                <Link to="/ref">REF</Link>
-                <Link to="/users">USERS</Link>
-                <Link to="/counter">COUNTER</Link>
-                <Link to="/calculator">CALCULATOR</Link>
-            </nav>
+        <div className={`app-container theme-${theme} rounded-md`}>
+            <BrowserRouter>
+                <nav className="bg-white rounded-md p-sm">
+                    <div className="flex gap-md justify-center align-center mb-sm">
+                        <Link to="/">HOME</Link>
+                        <Link to="/ref">REF</Link>
+                        <Link to="/users">USERS</Link>
+                        <Link to="/counter">COUNTER</Link>
+                        <Link to="/calculator">CALCULATOR</Link>
+                    </div>
 
-            <main className="container mx-auto p-md px-sm">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/ref" element={<RefPage />} />
-                    <Route path="/users" element={<UsersList />} />
-                    <Route path="/users/:userId" element={<UserPage />} />
-                    <Route path="/counter" element={<Counter />} />
-                    <Route path="/calculator" element={<Calculator />} />
-                </Routes>
-            </main>
+                    {/* Pulsante per cambiare tema */}
+                    <button onClick={toggleTheme}>Current theme: {theme}</button>
+                </nav>
 
-            <footer>Copyright @ 2026</footer>
-        </BrowserRouter>
+                <main className="container mx-auto p-md px-sm">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/ref" element={<RefPage />} />
+                        <Route path="/users" element={<UsersList />} />
+                        <Route path="/users/:userId" element={<UserPage />} />
+                        <Route path="/counter" element={<Counter />} />
+                        <Route path="/calculator" element={<Calculator />} />
+                    </Routes>
+                </main>
+
+                <footer>Copyright @ 2026</footer>
+            </BrowserRouter>
+        </div>
     );
 }
 
---
+++ src/providers/ThemeProvider.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import ThemeContext, { type Theme } from "../contexts/ThemeContext";
+
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+    const [theme, setTheme] = useState<Theme>("dark");
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeProvider;
